a3f804ae7dccb0ef8f687b327b060639cb881e9a
Alex Almeida <alex.almeida12@gmail.com>
2025-01-11 12:57:49 -0300

    Inserindo autenticação no Swagger

--- src/main/java/br/com/alexalmeida/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
+++ src/main/java/br/com/alexalmeida/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -2,7 +2,6 @@ package br.com.alexalmeida.gestao_vagas.modules.candidate.controllers;
 
 import br.com.alexalmeida.gestao_vagas.modules.candidate.DTO.AuthCandidateRequestDTO;
 import br.com.alexalmeida.gestao_vagas.modules.candidate.useCases.AuthCandidateUseCase;
-import br.com.alexalmeida.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.naming.AuthenticationException;
-
 @RestController
 @RequestMapping("/candidate")
-public class AutoCandidateController {
+public class AuthCandidateController {
 
     @Autowired
     private AuthCandidateUseCase authCandidateUseCase;
