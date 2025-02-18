cd1ed0a18ea6c2b7f34bebb7ac06bdc2579de411
Alex Almeida <alex.almeida12@gmail.com>
2025-02-17 22:36:11 -0300

    Criando entidade de Apply Job

--- src/main/java/br/com/alexalmeida/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/alexalmeida/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -3,6 +3,7 @@ package br.com.alexalmeida.gestao_vagas.modules.candidate.useCases;
 import br.com.alexalmeida.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.alexalmeida.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.alexalmeida.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.alexalmeida.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.alexalmeida.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ public class ApplyJobCandidateUseCase {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private ApplyJobRepository applyJobRepository;
 
 
     public void execute(UUID idCandidate, UUID idJob){
